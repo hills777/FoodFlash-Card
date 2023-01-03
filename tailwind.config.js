08f4c4e62885e3360a17a4b80ad683d2549ec2c7
hills777 <60585005+hills777@users.noreply.github.com>
2023-01-04 00:02:05 +0100

    added FF file
    index inside public folder

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html","./public/index.js"
+  ,"./public/test.html", "./public/test.js"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
